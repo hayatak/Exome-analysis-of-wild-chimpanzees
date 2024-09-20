838c064a27e659a49ae1d7540cef9c2b1d5090e4
Takashi Hayakawa <thawks.taxi@gmail.com>
2024-09-21 07:59:57 +0900

    Update 5.SNPEFF.sh

--- script/5.SNPEFF.sh
+++ script/5.SNPEFF.sh
@@ -8,4 +8,5 @@
 INDIV=
 java -Xmx64G -Xms64G -jar snpEff.jar \
 -stats ${INDIV}_snpEff_report.html \
-Pan_tro_3.0.105 ${INDIV}.vcf.gz > ${INDIV}_snpEff.vcf
+Pan_tro_3.0.105 ${INDIV}.vcf.gz \
+ > ${INDIV}_snpEff.vcf
