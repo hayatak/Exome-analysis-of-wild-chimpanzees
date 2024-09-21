cf1604c40abd266bd865b3227fef6839310980c5
Takashi Hayakawa <thawks.taxi@gmail.com>
2024-09-21 22:32:56 +0900

    Update 5.SNPEFF.sh

--- script/5.SNPEFF.sh
+++ script/5.SNPEFF.sh
@@ -3,9 +3,10 @@
 # Set INDIV name
 # e.g., INDIV=Bossou_FF45
 
+INDIV=
+
 # Start SNPEFF
 
-INDIV=
 java -Xmx64G -Xms64G -jar snpEff.jar \
 -stats ${INDIV}_snpEff_report.html \
 Pan_tro_3.0.105 ${INDIV}.vcf.gz \
